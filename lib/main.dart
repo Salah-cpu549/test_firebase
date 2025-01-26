3885815bead797a4e6c7bce295d8b893dd4689da
TheCodeFather <jhvhjjyhjk@sdfa>
2025-01-26 06:40:58 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main () => runApp((const NewApp()));
+
+class NewApp extends StatelessWidget {
+  const NewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
